0cd9c254361d0502946a158c393ed46c5359e1d2
zuotongxue <131460939+xxxcdr@users.noreply.github.com>
2023-05-03 23:52:32 +0800

    Update ChuanhuChatbot.py

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -477,3 +477,5 @@ if __name__ == "__main__":
     # demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", server_port=7860, share=False) # 可自定义端口
     # demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", server_port=7860,auth=("在这里填写用户名", "在这里填写密码")) # 可设置用户名与密码
     # demo.queue(concurrency_count=CONCURRENT_COUNT).launch(auth=("在这里填写用户名", "在这里填写密码")) # 适合Nginx反向代理
+    
+demo.launch(share=True)
